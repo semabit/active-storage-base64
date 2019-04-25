e0d395c331aef8737017c43eb2ef386c5b917a38
Shane Hofstetter <shane.hofstetter@gmail.com>
2019-04-25 11:47:43 +0200

    support rails 6
    (cherry picked from commit 90349b66535f248095b73ed2988fbcf7a5ff4911)

--- lib/active_storage_support/support_for_base64.rb
+++ lib/active_storage_support/support_for_base64.rb
@@ -8,24 +8,29 @@ module ActiveStorageSupport
       def has_one_base64_attached(name, dependent: :purge_later)
         has_one_attached name, dependent: dependent
 
-        add_helper_method(ActiveStorageSupport::Base64One, name, dependent: dependent)
+        add_helper_method(ActiveStorageSupport::Base64One, name)
       end
 
       def has_many_base64_attached(name, dependent: :purge_later)
         has_many_attached name, dependent: dependent
 
-        add_helper_method(ActiveStorageSupport::Base64Many, name, dependent: dependent)
+        add_helper_method(ActiveStorageSupport::Base64Many, name)
       end
 
-      def add_helper_method(type, name, dependent:)
+      def add_helper_method(type, name)
         class_eval <<-CODE, __FILE__, __LINE__ + 1
           def #{name}
             @active_storage_attached_#{name} ||=
-              #{type}.new("#{name}", self, dependent: #{dependent == :purge_later ? ':purge_later' : 'false'})
+              #{type}.new("#{name}", self)
           end
 
-          def #{name}=(data)
-            #{name}.attach(data)
+          def #{name}=(attachable)
+            attachment_changes["#{name}"] =
+              if attachable.nil?
+                ActiveStorage::Attached::Changes::DeleteOne.new("#{name}", self)
+              else
+                ActiveStorage::Attached::Changes::CreateOne.new("#{name}", self, ActiveStorageSupport::Base64Attach.attachment_from_data(attachable))
+              end
           end
         CODE
       end
